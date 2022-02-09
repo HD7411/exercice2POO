c81f9dcc9809f3b3614a1101174416e97ac4ad61
Hugo Duperthuy <hugo.duperthuy@outlook.fr>
2022-02-09 10:59:26 +0100

    Commit exo 3

---
+++ exercice3POO/exercice3POO/exercice3POO/Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercice3POO
+{
+    class Employe
+    {
+        private string matricule;
+        private string nom;
+        private string prenom;
+        private string dateNaissance;
+        private string dateEmbauche;
+        private int salaire;
+
+        public Employe(string matricule, string nom, string prenom, string dateNaissance, string dateEmbauche, int salaire)
+        {
+            this.matricule = matricule;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateNaissance = dateNaissance;
+            this.dateEmbauche = dateEmbauche;
+            this.salaire = salaire;
+        }
+        public string Matricule
+        {
+            get
+            {
+                return matricule;
+            }
+            set
+            {
+                matricule = value;
+            }
+        }
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                nom = value;
+            }
+        }
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+            set
+            {
+                prenom = value;
+            }
+        }
+        public string DateNaissance
+        {
+            get
+            {
+                return dateNaissance;
+            }
+            set
+            {
+                dateNaissance = value;
+            }
+        }
+        public string DateEmbauche
+        {
+            get
+            {
+                return dateEmbauche;
+            }
+            set
+            {
+                dateEmbauche = value;
+            }
+        }
+        public int Salaire
+        {
+            get
+            {
+                return salaire;
+            }
+            set
+            {
+                salaire = value;
+            }
+        }
+
+        
+        public void Age(string dateNaissance)
+        {
+            DateTime naissance = Convert.ToDateTime(dateNaissance);
+            int age = DateTime.Now.Year - naissance.Year;
+            DateTime DateAnniv = new DateTime(DateTime.Now.Year, naissance.Month, naissance.Day);
+            if (DateAnniv > DateTime.Now)
+            {
+                age--;
+            }
+            Console.WriteLine(age);
+        }
+        public void Anciennete(string dateEmbauche)
+        {
+            DateTime embauche = Convert.ToDateTime(dateEmbauche);
+        }
+    }
+    
+}
---
+++ exercice3POO/exercice3POO/exercice3POO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercice3POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matricule : ");
+            string matricule = Console.ReadLine();
+            Console.WriteLine("Nom : ");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Prenom : ");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Date de naissance : ");
+            string dateNaissance = Console.ReadLine();
+            Console.WriteLine("Date d'embauche : ");
+            string dateEmbauche = Console.ReadLine();
+            Console.WriteLine("Salaire : ");
+            int salaire = int.Parse(Console.ReadLine());
+
+            Employe employe = new Employe(matricule, nom, prenom, dateNaissance, dateEmbauche, salaire);
+            employe.Age(dateNaissance);
+        }
+    }
+}
